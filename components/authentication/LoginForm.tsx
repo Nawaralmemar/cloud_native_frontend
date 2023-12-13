d99638705c1dbc6c00fb5a9a09f97779199577fe
AlraweAbdullah <91222672+AlraweAbdullah@users.noreply.github.com>
2023-12-13 20:22:05 +0100

    Update LoginForm.tsx

--- components/authentication/LoginForm.tsx
+++ components/authentication/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, { use, useState } from 'react';
+import React, { useState } from 'react';
 import CustomerService from '../../services/CustomerService';
 import StatusMessageParser from '../StatusMessageParser';
 import { Customer } from '../../types';
